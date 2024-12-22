80db12d7c4b8f76a3d55ac965b47b4ac951d4bd5
ozmberkan <ozmberkan@gmail.com>
2024-12-22 16:30:12 +0300

    fixed some bugs

--- app/(dashboard)/hakkimizda/page.tsx
+++ app/(dashboard)/hakkimizda/page.tsx
@@ -4,7 +4,7 @@ const AboutPage = () => {
   return (
     <div>
       <h1 className="text-[75px] font-black bg-clip-text text-transparent bg-gradient-to-l from-slate-700 to-slate-900 ">
-        Hakkımızda
+        Hakkımızdaa
       </h1>
     </div>
   );
